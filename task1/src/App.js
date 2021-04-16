2b3a44adc9ed0799e8a3caa07d267edb59ba7537
Rossouw <rossouw.venter.rv@gmail.com>
2021-04-16 13:20:40 +0200

    Validating Email

--- task1/src/App.js
+++ task1/src/App.js
@@ -3,13 +3,14 @@ import Products from './Products.js';
 import { Button } from 'react-bootstrap';
 import Rating from './Rating.js';
 import JumboTronComponent from './JumboTronComponent';
+import UserFrom from './UserForm.js';
 
 class App extends Component {
   render() {
     const isValid = true;
   return (
     <div className="App">
-    <JumboTronComponent />
+    <UserFrom />
     </div>
   );
   }
