3e7cd7c736997a9f780f9760eb3bb92acb2925f7
Rossouw <rossouw.venter.rv@gmail.com>
2021-04-16 13:59:18 +0200

    Half of the github chapter

--- task1/src/App.js
+++ task1/src/App.js
@@ -4,13 +4,15 @@ import { Button } from 'react-bootstrap';
 import Rating from './Rating.js';
 import JumboTronComponent from './JumboTronComponent';
 import UserFrom from './UserForm.js';
+import GitHub from './GitHub.js';
+
 
 class App extends Component {
   render() {
     const isValid = true;
   return (
     <div className="App">
-    <UserFrom />
+    <GitHub />
     </div>
   );
   }
