01f8287fb457a85276a21cab61735dee244ea4a0
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-11 10:46:04 +0200

    Basics of JSX

--- Project 2/jsx/src/index.js
+++ Project 2/jsx/src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () =>{
-    return <div> Hi There!</div>;
+    const buttonText = 'Click me'
+    return <div>
+        <label className ="label" htmlFor="name">Enter name:</label>
+        <input id="name" type="text"/>
+        <button style={{BackgroundColor: 'blue', Color: 'white'}}>{buttonText}</button>
+        {/* Can be double or single quotes. */}
+    </div>
 };
 
 // Take the react component and show on the screen
