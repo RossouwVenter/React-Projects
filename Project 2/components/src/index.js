8555b1445283bcb15ab070b71abb2b5724a09c15
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-12 13:03:46 +0200

    Passing multiple props

--- Project 2/components/src/index.js
+++ Project 2/components/src/index.js
@@ -7,10 +7,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="Nice Blog!" src={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:30 PM" comment="Good work."/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 AM"comment="Keep up the hard work!"/>
+            {/* Giving a prop to component makes it unique. */}
         </div>
     );
 };
