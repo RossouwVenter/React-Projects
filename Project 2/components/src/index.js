e9d6a34bdfc60bcd1076d5e40bebbbe4bd162820
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-12 13:33:15 +0200

    Implementing cards on the page

--- Project 2/components/src/index.js
+++ Project 2/components/src/index.js
@@ -3,13 +3,15 @@ import reactDom from 'react-dom';
 // import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="Nice Blog!" src={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:30 PM" comment="Good work."/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 AM"comment="Keep up the hard work!"/>
+            <ApprovalCard />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="Nice Blog!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:30 PM" comment="Good work." avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 AM"comment="Keep up the hard work!" avatar={faker.image.avatar()}/>
             {/* Giving a prop to component makes it unique. */}
         </div>
     );
