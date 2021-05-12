e07b8762795481963a5798ba815ff872e40111c2
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-12 14:13:15 +0200

    Component reuse

--- Project 2/components/src/index.js
+++ Project 2/components/src/index.js
@@ -8,11 +8,24 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="Nice Blog!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:30 PM" comment="Good work." avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 AM"comment="Keep up the hard work!" avatar={faker.image.avatar()}/>
-            {/* Giving a prop to component makes it unique. */}
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="Nice Blog!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail author="Alex" timeAgo="Today at 2:30 PM" comment="Good work." avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 AM"comment="Keep up the hard work!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+                {/* Giving a prop to component makes it unique. */}
+            
+            {/* Passing the comment detail into the card  (the approval card.)*/}
         </div>
     );
 };
