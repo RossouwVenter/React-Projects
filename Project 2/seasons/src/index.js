7d7f7cda8aa4e357b04bdeef2bc543b500bd8ecb
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-13 12:31:09 +0200

    Handling error gracefully

--- Project 2/seasons/src/index.js
+++ Project 2/seasons/src/index.js
@@ -5,21 +5,33 @@ import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = { lat: null };
+        
+        // THIS IS THE ONLY TIME we do direct assignment to the this.state
+        this.state = { lat: null,errorMessage: '' };
         // initialize State object
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // Called setSate!!!!
+                this.setState({ lat: position.coords.latitude })
+            },
+            // Failure callback >
+            err => {
+                this.setState({errorMessage: err.message})
+            }
+        );
     }
 
 
     // We have to define render!!!
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            // Failure callback >
-            (err) => console.log(err)
+        return (
+        <div>
+            Latitude: {this.state.lat} 
+            <br /> 
+            Error: {this.state.errorMessage}
+        </div>
         );
-
-        return <div>Latitude: </div>;
     }
 }
 
