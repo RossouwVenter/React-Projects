2f3f419e540a9051e243a6de6908b16f49ed2920
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-14 10:30:42 +0200

    Showing icons

--- Project 2/seasons/src/SeasonDisplay.js
+++ Project 2/seasons/src/SeasonDisplay.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return    lat > 0 ? 'Summer' : 'Winter';
+    } else {
+        return    lat > 0 ? 'Winter' : 'Summer';
+    }
+}
+
 const SeasonDisplay = (props) =>{
-    console.log(props.lat)
-    return <div>Season display</div>
+    const season = getSeason(props.lat, new Date().getMonth())
+    const icon = season === 'Winter' ? 'snowflake': 'sun';
+
+    return <div><i className={`${icon} icon`}/><h1>{season === 'Winter' ? 'Burr, it is chilly': 'Lets hit the beach'}</h1><i className={`${icon} icon`}/></div>
 };
 
 export default SeasonDisplay;
