f159c9df047fdaf0891de41f839f36931f8a1704
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-19 10:25:17 +0200

    Section 6 udemy

--- Project 2/seasons/src/index.js
+++ Project 2/seasons/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {    
+class App extends React.Component {
     state = {lat: null, errorMessage: ''}
 
     componentDidMount() {
