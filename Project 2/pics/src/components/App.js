7f87ea33e084807607af14066562dc7b173e23c9
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-20 11:33:06 +0200

    viewing request results

--- Project 2/pics/src/components/App.js
+++ Project 2/pics/src/components/App.js
@@ -1,15 +1,22 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        // first part is the path.
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {query: term },
+            headers:{
+                Authorization: 'Client-ID wbUFv2Js1Jj44rvYmAS6IkkNshGq-XSyisvj1SMVYYw'
+            }
+        });
     }
 
     render() {
-        return (
-            
+        return (            
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
             </div>
