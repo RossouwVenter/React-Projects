ea6d3fdf73f05950953bbd8965fe2eb8d29c700c
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-25 12:29:38 +0200

    useState intro

--- Project 2/widget/src/App.js
+++ Project 2/widget/src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front en javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: ' React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 
 export default () => {
-    return <h1>Widgets app</h1>
+    return <div>
+        <br />
+        <Accordion items={items}/>
+    </div>
 };
