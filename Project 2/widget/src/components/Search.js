7e46d59a84191b4f0705087bab1ab3d6958ecbe9
Rossouw <rossouw.venter.rv@gmail.com>
2021-05-27 11:27:22 +0200

    async code in useeffect

--- Project 2/widget/src/components/Search.js
+++ Project 2/widget/src/components/Search.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm ] = useState('');
 
     useEffect (() => {
-        console.log('I run after every render');
+        const search = async() => {
+            await axios.get('')
+        }
+
+        search();
+        // runs code as soon as term changes.
     }, [term]);
 
     return (
