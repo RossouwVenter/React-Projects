07313ff98facdb2de0e6a7be16fdfc51848a935c
Rossouw <rossouw.venter.rv@gmail.com>
2021-06-17 11:48:00 +0200

    creating the translation

--- Project 2/widget/src/App.js
+++ Project 2/widget/src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
     {
         title: 'What is React?',
-        content: 'React is a front en javascript framework'
+        content: 'React is a front end javascript framework'
     },
     {
         title: 'Why use React?',
@@ -21,25 +22,23 @@ const items = [
 const options = [
     {
         label: 'The color Red',
-        value: 'red'
+        value: 'red',
     },
     {
         label: 'The color Green',
-        value: 'green'
+        value: 'green',
     },
     {
         label: 'The color Blue',
-        value: 'blue'
+        value: 'blue',
     },
 ];
-export default () => {
-    const [selected, setSelected] = useState(options[0]);
 
-    return <div>
-        <br />
-        <Dropdown 
-            selected={selected} 
-            onSelectedChange={setSelected}
-            options= {options}/>
-    </div>
+const App = () => {
+    return (
+        <div>
+            <Translate/>
+        </div>
+    );
 };
+export default App;
